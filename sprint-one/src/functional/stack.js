aec5bfa29fe4d71384e49d5c97138f6b9ef6366f
Justin <justinminh225@gmail.com>
2016-06-07 14:03:49 -0700

    Reports a size of 1 after adding two items and removing one

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,20 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var key = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    key++;
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
+    key--;
+    delete storage[key];
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
