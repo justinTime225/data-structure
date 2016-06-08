70251a1c99c4ebf4f040773248fd1e77e4fffa35
Justin <justinminh225@gmail.com>
2016-06-08 00:29:50 -0700

    Complete queue prototypal inheritance

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.last = 1;
+  obj.count = 1;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.last] = value;
+    this.last++;
+  },
+  dequeue: function() {
+    var item = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count++;
+    return item;
+  },
+  size: function() {
+    var result = this.last - this.count;
+    return result < 0 ? 0 : result;
+  }
+};
 
 
