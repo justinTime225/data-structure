f45ecddd1b8d7c533b98c763505547741dcbc260
Justin <justinminh225@gmail.com>
2016-06-10 15:53:03 -0700

    Complete hash table implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,20 +11,49 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var updated = false;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.set(index, [k, v]);
-  console.log(this._storage.get(index));
+  // if the bucket doesn't exist then create tuapl
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    // var bucket = this._storage.get(index);
+    // console.log(bucket);
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+        updated = true;
+      }
+    }
+    // if updated is false then push new array into bucket
+    if (!updated) {
+      bucket.push([k, v]);
+    }
+  }
+
 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  return bucket[1];
+  // bucket is an array of arrays
+  // loop over bucket to find the val you need
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
   
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
 };
 
 
