352b65f6183678919b0001de393b46bcb61a71f7
Justin <justinminh225@gmail.com>
2016-06-10 15:04:21 -0700

    should store values that were inserted ‣ should not contain values that were not inserted ‣ should overwrite values that have the same key

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,22 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+// storage will contains buckets that are accessed constant time
+// each bucket will contain arrays 
+
 HashTable.prototype.insert = function(k, v) {
+  var updated = false;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.set(index, [k, v]);
+  console.log(this._storage.get(index));
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  return bucket[1];
+  
 };
 
 HashTable.prototype.remove = function(k) {
